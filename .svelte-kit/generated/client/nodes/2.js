f35260df12ea81cabcc806965c6c4644acad5741
XavierLB <xavier.lebaux@gmail.com>
2023-10-25 14:35:52 +0200

    Clean up

--- .svelte-kit/generated/client/nodes/2.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/+page.svelte";
--- .svelte-kit/generated/client/nodes/5.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/sverdle/how-to-play/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/sverdle/how-to-play/+page.svelte";

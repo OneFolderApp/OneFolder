6080aaf22570fd7f83841cc4dcde11e8417ea552
XavierLB <xavier.lebaux@gmail.com>
2023-12-26 16:48:14 +0100

    delete annotation from metadata and tag

--- src/frontend/containers/ContentView/SlideMode/AnnotoriousWrapper.ts
+++ src/frontend/containers/ContentView/SlideMode/AnnotoriousWrapper.ts
@@ -37,15 +37,47 @@ class AnnotoriousWrapper {
 
     this.annotorious.on('createAnnotation', async (annotation: W3CAnnotation) => {
       const allAnotations = this.annotorious.getAnnotations();
+
+      const imageDimensions = await exifTool.getDimensions(this.file.absolutePath);
       const allMWGAnnotations = transformW3CAnnotationToMWGRegionInfo(
         allAnotations,
-        imgEl.width,
-        imgEl.height,
+        imageDimensions.width,
+        imageDimensions.height,
       );
       await exifTool.writeFacesAnnotations(this.file.absolutePath, allMWGAnnotations);
       const tagsToAdd = this.getTagsFromAnnotation(annotation.body);
       if (tagsToAdd[0]) {
         this.file.addPeopleTag(tagsToAdd[0]);
+      }
+    });
+
+    this.annotorious.on('deleteAnnotation', async (annotation: W3CAnnotation) => {
+      const allAnnotationsW3C = this.annotorious.getAnnotations();
+
+      const updatedAnnotationsW3C = allAnnotationsW3C.filter((annot) => {
+        return !(
+          annot['@context'] === annotation['@context'] &&
+          annot.type === annotation.type &&
+          JSON.stringify(annot.body) === JSON.stringify(annotation.body) &&
+          JSON.stringify(annot.target) === JSON.stringify(annotation.target)
+        );
+      });
+
+      const imageDimensions = await exifTool.getDimensions(this.file.absolutePath);
+      const allAnnotationsMWG = transformW3CAnnotationToMWGRegionInfo(
+        updatedAnnotationsW3C,
+        imageDimensions.width,
+        imageDimensions.height,
+      );
+      await exifTool.writeFacesAnnotations(this.file.absolutePath, allAnnotationsMWG);
+      this.annotorious.setAnnotations(updatedAnnotationsW3C);
+
+      const tagsToDelete = this.getTagsFromAnnotation(annotation.body);
+      if (tagsToDelete[0]) {
+        const tagFromTagStore = tagStore.findByName(tagsToDelete[0]);
+        if (tagFromTagStore) {
+          this.file.removeTag(tagFromTagStore);
+        }
       }
     });
   }
@@ -66,7 +98,6 @@ class AnnotoriousWrapper {
 
   destroy() {
     this.annotorious.destroy();
-    console.log('🗑️ Annotorious destroyed');
   }
 }
 

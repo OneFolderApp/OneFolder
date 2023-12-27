50e6e4af778d68d3aad50d08643a74a215c6a057
Antoine <27826950+Antoine-lb@users.noreply.github.com>
2023-12-27 18:31:06 +0100

    remove description from metadata panel to move it to it's own component

--- src/frontend/components/ImageInfo.tsx
+++ src/frontend/components/ImageInfo.tsx
@@ -49,7 +49,7 @@ const exifFields: Record<string, ExifField> = {
       return <ExternalLink url={url}>{url}</ExternalLink>;
     },
   },
-  ImageDescription: { label: 'Description', modifiable: true },
+  // ImageDescription: { label: 'Description', modifiable: true },
   Parameters: { label: 'Parameters' },
   Copyright: { label: 'Copyright', modifiable: true },
   Make: { label: 'Camera Manufacturer', modifiable: true },

16c78313c80b695d9124eabb2d3f12bbc0c3dab5
Antoine <27826950+Antoine-lb@users.noreply.github.com>
2024-01-09 15:29:43 +0100

    remove subLocation options when importing (to keep things simple)

--- src/frontend/containers/Outliner/LocationsPanel/LocationCreationDialog.tsx
+++ src/frontend/containers/Outliner/LocationsPanel/LocationCreationDialog.tsx
@@ -17,100 +17,100 @@ interface ITreeData {
   setExpansion: React.Dispatch<IExpansionState>;
 }
 
-const isExpanded = (nodeData: ClientLocation | ClientSubLocation, treeData: ITreeData) =>
-  !!treeData.expansion[nodeData instanceof ClientLocation ? nodeData.id : nodeData.path];
+// const isExpanded = (nodeData: ClientLocation | ClientSubLocation, treeData: ITreeData) =>
+//   !!treeData.expansion[nodeData instanceof ClientLocation ? nodeData.id : nodeData.path];
 
-const toggleExpansion = (nodeData: ClientLocation | ClientSubLocation, treeData: ITreeData) => {
-  const { expansion, setExpansion } = treeData;
-  if (nodeData instanceof ClientLocation) {
-    setExpansion({ ...expansion, [nodeData.id]: !expansion[nodeData.id] });
-  } else if (!nodeData.isExcluded) {
-    setExpansion({ ...expansion, [nodeData.path]: !expansion[nodeData.path] });
-  }
-};
+// const toggleExpansion = (nodeData: ClientLocation | ClientSubLocation, treeData: ITreeData) => {
+//   const { expansion, setExpansion } = treeData;
+//   if (nodeData instanceof ClientLocation) {
+//     setExpansion({ ...expansion, [nodeData.id]: !expansion[nodeData.id] });
+//   } else if (!nodeData.isExcluded) {
+//     setExpansion({ ...expansion, [nodeData.path]: !expansion[nodeData.path] });
+//   }
+// };
 
-const SubLocationLabel = ({
-  nodeData,
-  treeData,
-}: {
-  nodeData: ClientSubLocation;
-  treeData: ITreeData;
-}) => <SubLocation nodeData={nodeData} treeData={treeData} />;
+// const SubLocationLabel = ({
+//   nodeData,
+//   treeData,
+// }: {
+//   nodeData: ClientSubLocation;
+//   treeData: ITreeData;
+// }) => <SubLocation nodeData={nodeData} treeData={treeData} />;
 
-const mapDirectory = (dir: ClientSubLocation): ITreeItem => ({
-  id: dir.path,
-  label: SubLocationLabel,
-  nodeData: dir,
-  children: dir.subLocations.map(mapDirectory),
-  isExpanded,
-});
+// const mapDirectory = (dir: ClientSubLocation): ITreeItem => ({
+//   id: dir.path,
+//   label: SubLocationLabel,
+//   nodeData: dir,
+//   children: dir.subLocations.map(mapDirectory),
+//   isExpanded,
+// });
 
-const LocationLabel = (nodeData: any, treeData: any) => (
-  <Location nodeData={nodeData} treeData={treeData} />
-);
+// const LocationLabel = (nodeData: any, treeData: any) => (
+//   <Location nodeData={nodeData} treeData={treeData} />
+// );
 
-const SubLocation = observer(
-  ({ nodeData, treeData }: { nodeData: ClientSubLocation; treeData: ITreeData }) => {
-    const { expansion, setExpansion } = treeData;
-    const subLocation = nodeData;
+// const SubLocation = observer(
+//   ({ nodeData, treeData }: { nodeData: ClientSubLocation; treeData: ITreeData }) => {
+//     const { expansion, setExpansion } = treeData;
+//     const subLocation = nodeData;
 
-    const toggleExclusion = () => {
-      subLocation.toggleExcluded();
-      // Need to update expansion to force a rerender of the tree
-      setExpansion({ ...expansion, [subLocation.path]: false });
-    };
+//     const toggleExclusion = () => {
+//       subLocation.toggleExcluded();
+//       // Need to update expansion to force a rerender of the tree
+//       setExpansion({ ...expansion, [subLocation.path]: false });
+//     };
 
-    return (
-      <div className="tree-content-label">
-        <Checkbox onChange={toggleExclusion} checked={!subLocation.isExcluded}>
-          {subLocation.name}
-        </Checkbox>
-      </div>
-    );
-  },
-);
+//     return (
+//       <div className="tree-content-label">
+//         <Checkbox onChange={toggleExclusion} checked={!subLocation.isExcluded}>
+//           {subLocation.name}
+//         </Checkbox>
+//       </div>
+//     );
+//   },
+// );
 
-const Location = observer(({ nodeData }: { nodeData: ClientLocation; treeData: ITreeData }) => {
-  return (
-    <div className="tree-content-label">
-      <div>{nodeData.name}</div>
-    </div>
-  );
-});
+// const Location = observer(({ nodeData }: { nodeData: ClientLocation; treeData: ITreeData }) => {
+//   return (
+//     <div className="tree-content-label">
+//       <div>{nodeData.name}</div>
+//     </div>
+//   );
+// });
 
-const SubLocationInclusionTree = ({ location }: { location: ClientLocation }) => {
-  const [expansion, setExpansion] = useState<IExpansionState>({ [location.id]: true });
-  const treeData: ITreeData = useMemo<ITreeData>(
-    () => ({
-      expansion,
-      setExpansion,
-    }),
-    [expansion],
-  );
-  const [branches, setBranches] = useState<ITreeItem[]>([]);
+// const SubLocationInclusionTree = ({ location }: { location: ClientLocation }) => {
+//   const [expansion, setExpansion] = useState<IExpansionState>({ [location.id]: true });
+//   const treeData: ITreeData = useMemo<ITreeData>(
+//     () => ({
+//       expansion,
+//       setExpansion,
+//     }),
+//     [expansion],
+//   );
+//   const [branches, setBranches] = useState<ITreeItem[]>([]);
 
-  useAutorun(() => {
-    setBranches([
-      {
-        id: location.id,
-        label: LocationLabel,
-        children: location.subLocations.map(mapDirectory),
-        nodeData: location,
-        isExpanded,
-      },
-    ]);
-  });
+//   useAutorun(() => {
+//     setBranches([
+//       {
+//         id: location.id,
+//         label: LocationLabel,
+//         children: location.subLocations.map(mapDirectory),
+//         nodeData: location,
+//         isExpanded,
+//       },
+//     ]);
+//   });
 
-  return (
-    <Tree
-      id="new-location"
-      multiSelect
-      children={branches}
-      treeData={treeData}
-      toggleExpansion={toggleExpansion}
-    />
-  );
-};
+//   return (
+//     <Tree
+//       id="new-location"
+//       multiSelect
+//       children={branches}
+//       treeData={treeData}
+//       toggleExpansion={toggleExpansion}
+//     />
+//   );
+// };
 
 interface LocationCreationDialogProps {
   /** A new, un-initialized ClientLocation */
@@ -118,9 +118,8 @@ interface LocationCreationDialogProps {
   onClose: () => void;
 }
 
-const LocationCreationDialog = ({ location, onClose }: LocationCreationDialogProps) => {
-  const { locationStore } = useStore();
-  const [sublocationsLoaded, setSublocationsLoaded] = useState(false);
+const LocationCreationDialog = observer(({ location, onClose }: LocationCreationDialogProps) => {
+  const { locationStore, uiStore } = useStore();
   // const [importFolderHierarchyAsTags, setImportFolderHierarchyAsTags] = useState(false);
 
   const handleSubmit = useCallback(() => {
@@ -133,45 +132,35 @@ const LocationCreationDialog = ({ location, onClose }: LocationCreationDialogPro
     onClose();
   }, [location, onClose]);
 
-  useEffect(() => {
-    let isEffectRunning = true;
-    const dispose = when(
-      () => location.subLocations.length === 0 && !location.isInitialized,
-      () => {
-        location.refreshSublocations().then(() => isEffectRunning && setSublocationsLoaded(true));
-      },
-    );
-    return () => {
-      isEffectRunning = false;
-      dispose();
-    };
-  }, [location]);
+  // useEffect(() => {
+  //   let isEffectRunning = true;
+  //   const dispose = when(
+  //     () => location.subLocations.length === 0 && !location.isInitialized,
+  //     () => {
+  //       location.refreshSublocations().then(() => isEffectRunning && setSublocationsLoaded(true));
+  //     },
+  //   );
+  //   return () => {
+  //     isEffectRunning = false;
+  //     dispose();
+  //   };
+  // }, [location]);
 
   return (
     <Dialog
       open
-      title={`Add Location ${location.name}`}
+      title={`Add Location "${location.name}"?`}
       icon={IconSet.FOLDER_CLOSE}
       describedby="location-add-info"
       onCancel={handleCancel}
     >
       <p id="location-add-info">
-        You can configure the location {location.name} by including only certain subdirectories from
-        the directory.
-        {/* TODO: Switch for importing folder structure as tags */}
-        {/* <p>Would you like to create tags from the folder structure of this Location?</p>
-        <div style={{ marginLeft: '1rem' }}>
-          <Checkbox
-            label="Create tags from folder structure"
-            onChange={(e) => setImportFolderHierarchyAsTags(Boolean(e.target.checked))}
-            checked={importFolderHierarchyAsTags}
-          />
-        </div> */}
-        {/* Show folder, exclude directories */}
+        Once you click &quot;Confirm&quot;, OneFolder will generate thumnails, this may take a few
+        minutes to complete.
       </p>
       <form method="dialog" onSubmit={(e) => e.preventDefault()}>
-        <fieldset>
-          <legend>Included Subdirectories of {location.name}</legend>
+        {/* <fieldset>
+          <legend>Subdirectories:</legend>
           {!sublocationsLoaded ? (
             <i>{IconSet.LOADING} loading...</i>
           ) : location.subLocations.length === 0 ? (
@@ -179,8 +168,18 @@ const LocationCreationDialog = ({ location, onClose }: LocationCreationDialogPro
           ) : (
             <SubLocationInclusionTree location={location} />
           )}
-        </fieldset>
+        </fieldset> */}
 
+        <div>
+          <label htmlFor="import-metadata">Import metadata from images:</label>
+
+          <input
+            type="checkbox"
+            onChange={(e) => uiStore.setImportMetadataAtLocationLoading(Boolean(e.target.checked))}
+            checked={uiStore.importMetadataAtLocationLoading}
+            name="import-metadata"
+          />
+        </div>
         <fieldset className="dialog-actions">
           <Button styling="filled" text="Confirm" onClick={handleSubmit} />
           <Button styling="outlined" onClick={handleCancel} text="Cancel" />
@@ -188,6 +187,6 @@ const LocationCreationDialog = ({ location, onClose }: LocationCreationDialogPro
       </form>
     </Dialog>
   );
-};
+});
 
 export default LocationCreationDialog;

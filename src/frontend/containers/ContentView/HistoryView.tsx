fa80d0a12b0f3dfa2be9eaad7d9360b7f0059073
Antoine-lb <antoine.stc25@gmail.com>
2025-02-13 16:44:16 -0300

    empty history view

---
+++ src/frontend/containers/ContentView/HistoryView.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { GalleryProps } from './utils';
+
+const ListGallery = ({ contentRect, select, lastSelectionIndex }: GalleryProps) => {
+  return <div className="">History view</div>;
+};
+
+export default ListGallery;
